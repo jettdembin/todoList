c5ae5bdc5340484e3ff7ee1aa91aaf0606239b42
jettdembin <71148367+jettdembin@users.noreply.github.com>
2021-03-09 09:59:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 //Functions
 
@@ -33,4 +34,23 @@ function addTodo(event) {
     todoList.appendChild(todoDiv);
     //CLEAR TODOINPUT VALUE
     todoInput.value = "";
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    //DELETE TODO
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        //ANIMATION
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function() {
+            todo.remove();
+        });
+    }
+
+    //CHECKMARK
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
 }
